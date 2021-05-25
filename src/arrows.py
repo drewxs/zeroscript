f604451a0cd6a34c41666ce8be05d5f8212bb1c8
kito0 <drewshah0@gmail.com>
2021-05-25 01:11:25 -0600

    Format arrows

--- src/arrows.py
+++ src/arrows.py
@@ -3,10 +3,12 @@ def arrows(text, pos_start, pos_end):
 
     idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
     idx_end = text.find('\n', idx_start + 1)
+
     if idx_end < 0:
         idx_end = len(text)
 
     line_count = pos_end.ln - pos_start.ln + 1
+
     for i in range(line_count):
         line = text[idx_start:idx_end]
         col_start = pos_start.col if i == 0 else 0
@@ -17,6 +19,7 @@ def arrows(text, pos_start, pos_end):
 
         idx_start = idx_end
         idx_end = text.find('\n', idx_start + 1)
+
         if idx_end < 0:
             idx_end = len(text)
 
