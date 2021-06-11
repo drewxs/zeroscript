5b4bdaaddbbfd130ecae10bba9ef6b5c6d19b12f
kito0 <drewshah0@gmail.com>
2021-06-10 23:51:12 -0600

    Add constants for arithmetic operations

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,24 @@
+import string
+
 DIGITS = '0123456789'
+LETTERS = string.ascii_letters
+LETTERS_DIGITS = LETTERS + DIGITS
+
 TT_INT = 'INT'
 TT_FLOAT = 'FLOAT'
+TT_IDENTIFIER = 'IDENTIFIER'
+TT_KEYWORD = 'KEYWORD'
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
 TT_MUL = 'MUL'
 TT_DIV = 'DIV'
+TT_POW = 'POW'
+TT_EQ = 'EQ'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 TT_EOF = 'EOF'
+
+KEYWORDS = [
+    'var',
+    'let'
+]
